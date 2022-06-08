39ec8edd6d2ccb423a9a310d93044414204fcf11
mateuscavedini <mateuscvdn@gmail.com>
2022-06-08 03:34:21 -0300

    finished core code

--- scripts/script.js
+++ scripts/script.js
@@ -2,105 +2,104 @@ const btnNew = document.getElementById("new-color")
 const btnEasy = document.getElementById("easy")
 const btnHard = document.getElementById("hard")
 
-const prompt = document.getElementById("prompt")
+const display = document.getElementById("display")
 const squares = document.querySelectorAll(".square")
 const hardSquares = document.querySelectorAll(".hard")
 const result = document.getElementById("result")
 
 let selectedRGB
-
 let easyMode = true
-// let isCorresponding = false
+let isOver = false
 
+function reset() {
+    isOver = false
+    display.style.backgroundColor = "transparent"
+    result.classList.add("hidden")
+    result.textContent = "TRY AGAIN!"
+    btnNew.textContent = "NEW COLOR"
+}
 
 function colorGenerator() {
+    reset()
+    
     squares.forEach((sqr) => {
         let r = Math.round(Math.random() * 255)
         let g = Math.round(Math.random() * 255)
         let b = Math.round(Math.random() * 255)
         sqr.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
     })
+
     colorSelector()
+    toggleBtnColor()
+    toggleSqrVisibility()
 }
 
 function colorSelector() {
-    if (easyMode) {
-        let maxIndex = 2
-        selectedIndex = Math.round(Math.random() * maxIndex)
-    } else {
-        let maxIndex = 5
-        selectedIndex = Math.round(Math.random() * maxIndex)
-    }
+    let maxIndex
+    easyMode ? maxIndex = 2 : maxIndex = 5
+
+    let selectedIndex = Math.round(Math.random() * maxIndex)
     selectedRGB = squares[selectedIndex].style.backgroundColor
-    prompt.textContent = selectedRGB.toUpperCase()
-    prompt.style.backgroundColor = selectedRGB // << REMOVE *****
+    
+    display.textContent = selectedRGB.toUpperCase()
 }
 
 function toggleBtnColor() {
     if (easyMode) {
-        btnEasy.style.color = "black"
-        btnEasy.style.backgroundColor = "white"
-        btnHard.style.color = "white"
-        btnHard.style.backgroundColor = "transparent"
+        btnEasy.classList.add("pressed")
+        btnHard.classList.remove("pressed")
     } else {
-        btnEasy.style.color = "white"
-        btnEasy.style.backgroundColor = "transparent"
-        btnHard.style.color = "black"
-        btnHard.style.backgroundColor = "white"
+        btnEasy.classList.remove("pressed")
+        btnHard.classList.add("pressed")
     }
 }
 
-function toggleVisibility() {
-    hardSquares.forEach((sqr) => {
-        if (easyMode) {
-            return sqr.style.visibility = "hidden"
-        } else {
-            return sqr.style.visibility = "visible"
-        }
+function toggleSqrVisibility() {
+    squares.forEach((sqr) => sqr.classList.remove("remove"))
+
+    hardSquares.forEach((sqr) => easyMode ? sqr.classList.add("hidden") : sqr.classList.remove("hidden"))
+}
+
+function endGame() {
+    isOver = true
+    btnNew.textContent = "PLAY AGAIN"
+    result.textContent = "CORRECT!"
+    result.classList.remove("hidden")
+    display.style.backgroundColor = selectedRGB
+    
+    squares.forEach((sqr) => {
+        sqr.classList.remove("remove")
+        sqr.style.backgroundColor = selectedRGB
     })
 }
 
+
 window.addEventListener("load", () => {
     easyMode = true
-    btnEasy.style.color = "black"
-    btnEasy.style.backgroundColor = "white"
     colorGenerator()
 })
+
 btnNew.addEventListener("click", colorGenerator)
 btnEasy.addEventListener("click", () => {
     easyMode = true
-    toggleBtnColor()
-    toggleVisibility()
     colorGenerator()
 })
 btnHard.addEventListener("click", () => {
     easyMode = false
-    toggleBtnColor()
-    toggleVisibility()
     colorGenerator()
 })
 
-// squares.forEach((sqr) => {
-//     sqr.addEventListener("click", () => {
-//         console.log(sqr)
-        
-//         if (selectedRGB == sqr.style.backgroundColor) {
-//             result.textContent = "CORRECT!"
-//             result.style.visibility = "visible"
-//             return prompt.style.backgroundColor = selectedRGB
-//         }
-//         sqr.style.transition = "0.4s ease-in"
-//         sqr.style.visibility = "hidden"
-//     })
-// })
+squares.forEach((sqr) => {
+    if (isOver) {
+        return
+    }
 
-/*
-
-css classlist example
-.transition {
-  transition: 0.5s ease-in-out;
-  opacity: 0;
-  visibility: hidden;
-}
-
-*/
+    sqr.addEventListener("click", () => {
+        if (selectedRGB != sqr.style.backgroundColor) {
+            sqr.classList.add("remove")
+            return result.classList.remove("hidden")
+        }
+    
+        endGame()
+    })
+})
